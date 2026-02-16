7dfa207001c1b9292d44854419d88f2245a827a0
Rishabh Raj <225162739+rishabhraj2511@users.noreply.github.com>
2026-02-16 11:16:59 +0530

    Create Fractional Knapsack.java

---
+++ DAA /CodeTantra/Fractional Knapsack.java
@@ -0,0 +1,2 @@
+// Given n items, each with a given weight and value, you need to determine the maximum total value 
+// that can be obtained by placing these items into a knapsack with a capacity of w.
